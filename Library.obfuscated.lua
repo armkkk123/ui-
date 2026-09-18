42aba6559c6210c9662b857644b2c4760a0f9dc6
suphawat Wibunphan <armzyarm97@gmail.com>
2026-09-18 21:31:29 +0700

    Update Library.obfuscated.lua

--- Library.obfuscated.lua
+++ Library.obfuscated.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/armkkk123/6857010160830e77bc127d141744ecfb/raw/library.lua"))()
+return loadstring(game:HttpGet('https://gist.githubusercontent.com/armkkk123/6857010160830e77bc127d141744ecfb/raw/library.lua'))()
